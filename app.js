e4d6d032ae871e4c8f1bbcb8fa658d7c0f6461d7
dannertech <danner@danner.tech>
2018-10-08 12:25:46 -0400

    added URI to .env file

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
 
 const connection = mongoose.connection;
 connection.on('connected', () => {

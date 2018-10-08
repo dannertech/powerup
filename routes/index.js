6d5aaa60c20e111f829c860958c29afe76c6d1a3
dannertech <danner@danner.tech>
2018-10-08 16:22:20 -0400

    edited usersController page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log("this is the users homepage showing all of the users");
 });
 
 module.exports = router;

5bd0caee0e090240bcab6575697dea1bc487a314
dannertech <danner@danner.tech>
2018-10-08 14:10:06 -0400

    updated app.js file to include new routes based on build directory

--- app.js
+++ app.js
@@ -26,13 +26,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(__dirname + '/client/build/'));
+  app.use(express.static(__dirname + '/client/build/'));
 
-app.get('/', (req,res) => {
+  app.get('/', (req,res) => {
     res.sendFile(__dirname + '/client/build/index.html')
-  })
+})
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 
 module.exports = app;

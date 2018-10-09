880e925d966024dae4fb0b66da31acddbe448db3
dannertech <danner@danner.tech>
2018-10-09 14:38:32 -0400

    added form to signup page

--- client/src/components/signupPage/signupPage.js
+++ client/src/components/signupPage/signupPage.js
@@ -7,7 +7,21 @@ export default class SignupPage extends Component {
         <h1>Sign Up</h1>
         <form>
           <label>Name</label>
+          <br>
+          </br>
           <input type='text' value="Name"></input>
+          <br>
+          </br>
+          <label>Username</label>
+          <br>
+          </br>
+          <input type='text' value="Username"></input>
+          <br>
+          </br>
+          <label>Email</label>
+          <br>
+          </br>
+          <input type='text' value='Email'></input>
         </form>
 
       </div>

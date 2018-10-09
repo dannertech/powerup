37fa298de554dcb58ba7336c01941f660768a87c
dannertech <danner@danner.tech>
2018-10-09 19:40:40 -0400

    imported users list into loginpage

--- client/src/components/loginPage/loginPage.js
+++ client/src/components/loginPage/loginPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import styled from 'styled-components'
+import UsersList from '../UsersList';
 
 export default class LoginPage extends Component {
   state = {
@@ -22,6 +23,7 @@ users: []
       <div>
         <h1>This is the login page</h1>
         <h5>Select Your Account</h5>
+        <UsersList />
       </div>
     )
   }
